5786e2ac92842411296440edbc7e09c419af8d58
beneficial-bundle-8122 <satyasinha94@gmail.com>
2018-10-05 17:16:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ function whisper(string) {
 function logShout(string) {
   console.log(string.toUpperCase())
 }
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if string === string.toUpperCase() {
+    return "YES INDEED!"
+  }
+}
