9c72d0078d0c12a8bf7bbf79300841c4f2783b07
beneficial-bundle-8122 <satyasinha94@gmail.com>
2018-10-05 17:19:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   }
 
-  if (string === string.toUpperCase && string == "I love you, Grandma.") {
+  if (string == "I love you, Grandma.") {
     return "I love you too."
   }
 }
